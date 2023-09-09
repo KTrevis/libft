001f376c5202b07efbac4cbd3120698ef15a8f77
trevis <treviskevin77@gmail.com>
2023-09-09 18:41:57 +0200

    forgot atoi, fixed

---
+++ src/ft_atoi.c
@@ -0,0 +1,25 @@
+#include "libft.h"
+
+int	ft_atoi(char *str)
+{
+	int	n;
+	int	sign;
+
+	n = 0;
+	sign = 1;
+	while (*str && ((*str >= 9 && *str <= 13) || *str == ' '))
+		str++;
+	if (*str == '-')
+	{
+		sign = -sign;
+		str++;
+	}
+	if (*str == '+')
+		str++;
+	while (*str && *str >= '0' && *str <= '9')
+	{
+		n = n * 10 + *str - '0';
+		str++;
+	}
+	return (n * sign);
+}
