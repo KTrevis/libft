134a1e107fafffc56f78e6d42f8dfffcef931e24
Kevin <treviskevin77@gmail.com>
2023-09-10 19:20:44 +0200

    chained list update

--- bonus/ft_lstnew.c
+++ bonus/ft_lstnew.c
@@ -1,29 +1,55 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_lstnew.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/09 19:00:38 by ketrevis          #+#    #+#             */
-/*   Updated: 2023/09/10 17:11:00 by marvin           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
+#include <stdio.h>
 
 t_list	*ft_lstnew(void *content)
 {
-	t_list	*curr;
+	t_list	*new;
 
-	curr = malloc(sizeof(t_list));
-	curr->next = NULL;
-	curr->content = content;
-	return (curr);
+	new = malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
+	new->next = NULL;
+	new->content = content;
+	return (new);
 }
 
 void ft_lstadd_front(t_list **alst, t_list *new)
 {
 	new->next = *alst;
-	*alst = 
+	*alst = new;
+}
+
+int	ft_lstsize(t_list *lst)
+{
+	int	size;
+
+	size = 0;
+	while (lst != NULL)
+	{
+		lst = lst->next;
+		size++;
+	}
+	return (size);
+}
+
+int main(int ac, char **av)
+{
+	t_list *head = ft_lstnew((void *)10);
+	for (int i = 0; i < 10; i++) {
+		int *value = malloc(sizeof(int));
+		*value = i;
+		t_list *new = ft_lstnew(value);
+		ft_lstadd_front(&head, new);
+	}
+
+	t_list *current = head;
+	while (current->next != NULL)
+	{
+		printf("%d\n", *((int *)current->content));
+		current = current->next;
+	}
+	printf("%d\n", ft_lstsize(head));
+
+	printf("test");
+	return (0);
 }
