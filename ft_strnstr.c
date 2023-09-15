a900736707c8fab59bc3adf82cb46b7ed6507648
Kevin <treviskevin77@gmail.com>
2023-09-15 11:31:08 +0200

    test + fix strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,13 +1,13 @@
 #include "libft.h"
 
-static int	is_substr(const char *big, const char *little)
+static size_t	is_substr(const char *big, const char *little)
 {
 	size_t	i;
 
 	i = 0;
 	while (big[i] && big[i] == little[i])
 		i++;
-	return (ft_strlen(big) == i);
+	return (ft_strlen(little) == i);
 }
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
@@ -15,8 +15,12 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	i;
 
 	i = 0;
+	if (*little == '\0')
+		return ((char *)big);
 	while (big[i] && i < len)
 	{
+		if (i + ft_strlen(little) > len)
+			return (NULL);
 		if (is_substr(&big[i], little))
 			return ((char *)&big[i]);
 		i++;
--- tests/test_strnstr.c
+++ tests/test_strnstr.c
@@ -1,10 +1,10 @@
-#include "libft.h"
 #include <stdio.h>
-#include <string.h>
+#include <bsd/string.h>
+#include "libft.h"
 
 int	main(int ac, char **av)
 {
 	(void)ac;
-	printf("%s\n", ft_strnstr(av[1], av[2], atoi(av[3])));
-	printf("%s\n", strnstr(av[1], av[2], atoi(av[3])));
+	printf("%p\n", ft_strnstr(av[1], av[2], atoi(av[3])));
+	printf("%p\n", strnstr(av[1], av[2], atoi(av[3])));
 }
